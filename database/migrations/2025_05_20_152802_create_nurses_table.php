29831345058f83282f2538c77556a22ebbb98ca8
Rover track <rishan.pgowda@gmail.com>
2025-05-21 23:16:23 +0530

    migrations

---
+++ database/migrations/2025_05_20_152802_create_nurses_table.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('nurses', function (Blueprint $table) {
+            $table->foreignId('id')->constrained('users')->onDelete('cascade');
+            $table->string('qualification');
+            $table->integer('experience_years');
+            $table->text('bio');
+            $table->timestamps();
+        });
+        Schema::create('services', function (Blueprint $table) {
+            $table->id();
+            $table->string('service_name');
+            $table->text('service_description');
+            $table->timestamps();
+        });
+        Schema::create('bookings', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('patient_id')->constrained('users')->onDelete('cascade');
+            $table->foreignId('nurse_id')->constrained('users')->onDelete('cascade');
+            $table->foreignId('service_id')->constrained('services')->onDelete('cascade');
+            $table->enum('booking_type', ['instant', 'long_term', 'caregiver']);
+            $table->enum('status', ['pending', 'ongoing', 'completed', 'cacelled']);
+            $table->dateTime('scheduled_time');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('nurses');
+    }
+};
